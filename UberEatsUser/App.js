683a2fdc420eeadafc2ebf3f580d33a61fea3c25
sreedevi111 <sreedevisobhana@gmail.com>
2022-05-26 10:03:41 +0530

    FlatList updated

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import RestaurantItem from "./src/components/RestaurantItem";
-import restaurants from './assets/data/restaurants.json'
-
-
+import restaurants from "./assets/data/restaurants.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* Restaurant item */}
-     
 
-    <RestaurantItem restaurant = {restaurants[0]} />
-    <RestaurantItem restaurant = {restaurants[1]} />
+      {/* <RestaurantItem restaurant = {restaurants[0]} />
+    <RestaurantItem restaurant = {restaurants[1]} /> */}
 
-   
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+        showsVerticalScrollIndicator={false}
+      />
 
       <StatusBar style="auto" />
     </View>
@@ -27,7 +28,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    padding:10
+    padding: 10,
+    paddingVertical: 30,
   },
-  
 });
