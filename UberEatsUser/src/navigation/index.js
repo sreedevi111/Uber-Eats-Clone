0ac1acfdbf7ae2ce0484c6acec8745a9587b1d28
sreedevi111 <sreedevisobhana@gmail.com>
2022-05-31 12:12:02 +0530

    navigation modified

--- UberEatsUser/src/navigation/index.js
+++ UberEatsUser/src/navigation/index.js
@@ -1,26 +1,20 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { Foundation } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { MaterialIcons } from '@expo/vector-icons';
-import BasketDishItem from "../../src/components/BasketDishItem";
-import OrderDetails from "../../src/components/OrderListItem";
-import OrdersScren from "../../src/components/OrderListItem";
-import DishDetailScreen from "../../src/screens/DishDetailsScreen";
-import HomeScreen from "../../src/screens/HomeScreen";
-import RestaurantDetailsPage from "../../src/screens/RestaurantDetailsScreen";
+import HomeScreen from "../screens/HomeScreen";
+import RestaurantDetailsScreen from "../screens/RestaurantDetailsScreen";
+import DishDetailsScreen from "../screens/DishDetailsScreen";
+import Basket from "../screens/Basket";
+import OrdersScreen from "../screens/OrdersScreen";
+import OrderDetails from "../screens/OrderDetails";
+
+import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeTabs} />
-      <Stack.Screen
-        name="Restaurant"
-        component={RestaurantDetailsPage}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="HomeTabs" component={HomeTabs} />
     </Stack.Navigator>
   );
 };
@@ -29,36 +23,60 @@ const Tab = createMaterialBottomTabNavigator();
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator barStyle = {{backgroundColor:"white"}}>
+    <Tab.Navigator barStyle={{ backgroundColor: "white" }}>
       <Tab.Screen
         name="Home"
-        component={HomeScreen}
+        component={HomeStackNavigator}
         options={{
           tabBarIcon: ({ color }) => (
             <Foundation name="home" size={24} color={color} />
           ),
         }}
       />
-
       <Tab.Screen
-       name="Orders"
-        component={OrderDetails}
-        options ={{tabBarIcon: ({color}) =>(
+        name="Orders"
+        component={OrderStackNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
             <MaterialIcons name="list-alt" size={24} color={color} />
-        )}}
-        
-        />
-
-
-      <Tab.Screen name="Profile" component={HomeScreen}
-      options ={{
-        tabBarIcon:({color})=>(
-        <FontAwesome5 name="user-alt" size={24} color={color} />
-) }}
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={OrdersScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user-alt" size={24} color={color} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
 };
 
+const HomeStack = createNativeStackNavigator();
+
+const HomeStackNavigator = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Restaurants" component={HomeScreen} />
+      <HomeStack.Screen name="Restaurant" component={RestaurantDetailsScreen} />
+      <HomeStack.Screen name="Dish" component={DishDetailsScreen} />
+      <HomeStack.Screen name="Basket" component={Basket} />
+    </HomeStack.Navigator>
+  );
+};
+
+const OrdersStack = createNativeStackNavigator();
+
+const OrderStackNavigator = () => {
+  return (
+    <OrdersStack.Navigator>
+      <OrdersStack.Screen name="Orders" component={OrdersScreen} />
+      <OrdersStack.Screen name="Order" component={OrderDetails} />
+    </OrdersStack.Navigator>
+  );
+};
+
 export default RootNavigator;
-// export default HomeTabs;
