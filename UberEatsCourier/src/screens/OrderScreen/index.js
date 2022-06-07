7b0585880ef4a2f997fc0f5e550637ad29a72e12
sreedevi111 <sreedevisobhana@gmail.com>
2022-06-07 21:39:38 +0530

    implement map

--- UberEatsCourier/src/screens/OrderScreen/index.js
+++ UberEatsCourier/src/screens/OrderScreen/index.js
@@ -1,17 +1,52 @@
 import { useRef, useMemo } from "react";
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  useWindowDimensions,
+} from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../../assets/orders.json";
 import OrderItem from "../../components/OrderItem";
+import MapView, { Marker } from "react-native-maps";
+import { Entypo } from "@expo/vector-icons";
 
 const OrderScreen = () => {
   const bottomSheetRef = useRef(null);
-  const snapPoints = useMemo(()=>["12%","95%"],[])
+  const { width, height } = useWindowDimensions();
+  const snapPoints = useMemo(() => ["12%", "95%"], []);
 
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
+      <MapView
+        style={{
+          height,
+          width,
+        }}
+        showsUserLocation
+        followsUserLocation
+      >
+        {/* marker for each order */}
+        {orders.map((order) => (
+          <Marker
+          key = {order.id}
+            title={order.Restaurant.name}
+            description={order.Restaurant.address}
+            coordinate={{
+              latitude: order.Restaurant.lat,
+              longitude:order.Restaurant.lng,
+            }}
+          >
+            <View
+              style={{ backgroundColor: "green", padding: 5, borderRadius: 20 }}
+            >
+              <Entypo name="shop" size={24} color="white" />
+            </View>
+          </Marker>
+        ))}
+      </MapView>
       <BottomSheet ref={bottomSheetRef} index={0} snapPoints={snapPoints}>
-        <View style={{ flex: 1, alignItems: "center", marginBottom:30 }}>
+        <View style={{ flex: 1, alignItems: "center", marginBottom: 30 }}>
           <Text
             style={{
               fontSize: 20,
@@ -25,12 +60,11 @@ const OrderScreen = () => {
           <Text style={{ letterSpacing: 0.5 }}>
             Available orders:{orders.length}
           </Text>
-          
         </View>
-        <FlatList 
-    data = {orders}
-    renderItem ={({item}) => <OrderItem order = {item}/>}
-    />
+        <FlatList
+          data={orders}
+          renderItem={({ item }) => <OrderItem order={item} />}
+        />
       </BottomSheet>
     </View>
   );
